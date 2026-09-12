328f97feff70c696497dc25e198241aec7899b95
Arushi Kapur <kapur.aru12@gmail.com>
2026-09-13 01:36:16 +0530

    Gatway Setup and initialize auth service

---
+++ backend/gateway/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const port = process.env.PORT
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.json({message:"hello from gateway"})
+})
+
+app.listen(port,()=>{
+    console.log(`gateway started at ${port}`)
+})
