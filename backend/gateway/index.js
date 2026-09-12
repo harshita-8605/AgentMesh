0ae6b9c653dd3d75b9a3ee63dde27867555c992e
Harshita <harshita@Harshitas-MacBook-Air-3.local>
2026-09-13 01:36:01 +0530

    gateway setup and initialized auth service

---
+++ backend/gateway/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const port = process.env.PORT
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({ message: "hello from gateway, I am under the water" })
+})
+
+app.listen(port, () =>{
+    console.log(`gateway started at ${port}`)
+})
+
